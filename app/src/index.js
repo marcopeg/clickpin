136b286a0d9551993dee970413cf19e53999a7a6
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-04-12 09:43:09 +0000

    Add basic frontend scaffold
    #5

--- app/src/index.js
+++ app/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { CssBaseline } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const theme = createMuiTheme();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <React.Fragment>
+        <CssBaseline />
+        <App />
+      </React.Fragment>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
