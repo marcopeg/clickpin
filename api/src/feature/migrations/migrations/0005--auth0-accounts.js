a2e4a92e7b4fa8675d97476deaf1f5471d027c29
Marco Pegoraro <marco.pegoraro@gmail.com>
2020-04-13 10:48:04 +0000

    Add accounts table and Auth0 rule
    #8

---
+++ api/src/feature/migrations/migrations/0005--auth0-accounts.js
@@ -0,0 +1,74 @@
+const ACCOUNTS_CREATE = `
+  CREATE TABLE public.accounts (
+    id BIGSERIAL PRIMARY KEY,
+    uuid UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    auth0_id VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    is_active BOOL NOT NULL DEFAULT true,
+    is_confirmed BOOL NOT NULL DEFAULT false,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE,
+    data JSONB NOT NULL DEFAULT '{}'
+  );
+`;
+
+const ACCOUNTS_DROP = `
+  DROP TABLE IF EXISTS public.accounts CASCADE;
+`;
+
+const ACCOUNTS_POPULATE = `
+  INSERT INTO public.accounts 
+  (id, email, auth0_id)
+  VALUES
+  (1, 'dev@wallie.com', 'dev::dev')
+`;
+
+const down = async hasura => {
+  console.log('DOWN ----------------------------------->');
+  await hasura.untrackTable({
+    schema: 'public',
+    name: 'accounts',
+    cascade: true,
+  });
+
+  await hasura.query(ACCOUNTS_DROP, null, { throw: false, log: 'dismantle' });
+  await hasura.query(`DROP EXTENSION IF EXISTS "uuid-ossp";`);
+};
+
+const up = async hasura => {
+  await down(hasura);
+  console.log('  UP ----------------------------------->');
+  await hasura.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
+  await hasura.query(ACCOUNTS_CREATE, null, { throw: false, log: 'build' });
+
+  await hasura.trackTable({
+    schema: 'public',
+    name: 'accounts',
+  });
+
+  await hasura.createSelectPermission({
+    role: 'user',
+    table: 'accounts',
+    permission: {
+      columns: [
+        'uuid',
+        'email',
+        'is_active',
+        'is_confirmed',
+        'data',
+        'created_at',
+        'updated_at',
+        'last_login_at',
+      ],
+      filter: { uuid: { _eq: 'X-Hasura-User-Id' } },
+    },
+  });
+
+  await hasura.query(ACCOUNTS_POPULATE, null, { throw: false, log: 'build' });
+};
+
+module.exports = {
+  stable: false,
+  up,
+  down,
+};
---
+++ docs/auth0/rules/sync-account-auth0-to-hasura.js
@@ -0,0 +1,74 @@
+/**
+ * Synchronizes the user's account details to Hasura and
+ * enriches the accessToken for Hasura authenticated queries.
+ * @param {*} user 
+ * @param {*} context 
+ * @param {*} callback 
+ */
+
+// eslint-disable-next-line
+function (user, context, callback) {  
+  const query = `
+    mutation($userId: String!, $email: String) {
+      insert_accounts(
+        objects: [{
+            auth0_id: $userId
+            email: $email
+						updated_at: "now()"
+        }],
+        on_conflict: {
+            constraint: accounts_auth0_id_key,
+            update_columns: [updated_at]
+        }
+      ) {
+        affected_rows
+        returning { id }
+      }
+    }
+  `;
+
+  const variables = {
+    userId: user.user_id,
+    email: user.email,
+  };
+
+  const payload = {
+    headers: {
+      "content-type": "application/json",
+      "x-hasura-admin-secret": configuration.HASURA_ADMIN_SECRET
+    },
+    url: `${configuration.HASURA_BASE_URL}/v1/graphql`,
+    body: JSON.stringify({ query, variables }),
+  };
+
+  request.post(payload, (error, response, body) => {
+    // Eager return in case of errors
+  	if (error) return callback(error, user, context);
+
+    // Try to grab the user's id from the response
+    let userId = null;
+    try {
+      const data = JSON.parse(body);
+			const users = data.data.insert_accounts.returning;
+      if (!users.length) throw new Error('Missing user');
+      userId = users[0].id;
+    } catch (err) {
+      return callback(err, user, context);
+    }
+    
+    // Decorate the access token for JWT authenticated requests
+    try {
+      context.accessToken['https://hasura.io/jwt/claims'] = {
+        "x-hasura-default-role": "user",
+        "x-hasura-allowed-roles": ["user"],
+        "x-hasura-user-id": userId,
+        // "x-auth0-user-id": user.user_id,
+      };
+
+      // console.log(context.accessToken);
+      callback(null, user, context);
+    } catch (err) {
+      callback(err, user, context);
+    }
+  });
+}
